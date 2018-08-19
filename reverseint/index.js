d4066646222c3cd7792967f8a3e2e8d9ac8d999f
Jakub Skoneczny <skoneczny.j@gmail.com>
2018-08-19 11:43:01 +0200

    reverseint solution

--- reverseint/index.js
+++ reverseint/index.js
@@ -8,6 +8,16 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  // int to arr of strings
+  let arr = n.toString().split("");
+  // reverse arr
+  arr.reverse();
+  // join arr to string, string to int
+  let result = parseInt(arr.join(""));
+  // return with sign
+  result *= Math.sign(n);
+  return result;
+}
 
 module.exports = reverseInt;
