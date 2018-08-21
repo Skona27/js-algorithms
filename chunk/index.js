08f17c8b1e5eaad1f01288100021f7a3475c54bc
Jakub Skoneczny <skoneczny.j@gmail.com>
2018-08-21 20:21:15 +0200

    array chunk

--- chunk/index.js
+++ chunk/index.js
@@ -8,6 +8,25 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  let result = [];
+  let tempArr = [];
+
+  array.forEach((el, i) => {
+    // if index is divisible by arrSize, then it should go to new array
+    if(i && i % size === 0) {
+      // push current tempArr
+      result.push(tempArr);
+      // clear tempArr
+      tempArr = [];
+    } 
+      // push element to tempArr
+      tempArr.push(el);
+  });
+
+  // beforeend, push last tempArr and return
+  result.push(tempArr);
+  return result;
+}
 
 module.exports = chunk;
