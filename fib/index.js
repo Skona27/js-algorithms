927809869334bef1358c49b0691ebfff3cee60ae
Jakub Skoneczny <skoneczny.j@gmail.com>
2018-08-24 11:10:55 +0000

    memoized fibonacci

--- fib/index.js
+++ fib/index.js
@@ -8,12 +8,29 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
+function slowFib(n) {
     if (n < 2) {
         return n;
     }
     
     return fib(n-2) + fib(n-1);
 }
+
+function memoize(fn) {
+    const cache = {};
+    
+    return function(...args) {
+        if (cache[args]) {
+            return cache[args]
+        }
+        
+        const result = fn.apply(this, args);
+        cache[args] = result;
+        
+        return result;
+    }
+}
+
+const fib = memoize(slowFib);
 
 module.exports = fib;
