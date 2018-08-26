1d22d9d235c3d17027ae4aa677e735a45ac191ff
Jakub Skoneczny <skoneczny.j@gmail.com>
2018-08-26 12:58:55 +0200

    stack implementation

--- stack/index.js
+++ stack/index.js
@@ -10,6 +10,23 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack {}
+class Stack {
+  constructor() {
+    this.data = [];
+  }
+
+  push(item) {
+    this.data.push(item);
+  }
+
+  pop() {
+    return this.data.pop();
+  }
+
+  peek() {
+    let lastInd = this.data.length - 1;
+    return this.data[lastInd];
+  }
+}
 
 module.exports = Stack;
