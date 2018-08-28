fec26b3b09a35f60a1ba618d45e9bb3bd409d0b9
Jakub Skoneczny <skoneczny.j@gmail.com>
2018-08-28 18:00:19 +0200

    linked list part1

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -2,8 +2,55 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(data) { 
+    this.head = new Node(data, this.head);
+  }
+
+  size() {
+    let node = this.head;
+    let count = 0;
+
+    while (node) {
+      count++;
+      node = node.next;
+    }
+
+    return count;
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    let node = this.head;
+
+    while (node.next) {
+      node = node.next;
+    }
+
+    return node;
+  }
+
+  clear() {
+    this.head = null;
+  }
+
+  removeFirst() {
+    this.head = this.head.next;
+  }
+}
 
 module.exports = { Node, LinkedList };
